d365eefb14c432934befd261a126c097f2f88d57
Hawa <sandyhawa6@gmail.com>
2023-09-20 03:22:25 +0000

    forth commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,12 @@
   import Signin from "./signin/+page.svelte";
   import Signup from "./signup/+page.svelte";
   import Navbar from "./Navabar/+page.svelte";
+  import Landingpage from "./Landingpage/+page.svelte"
 
 
 </script>
 <!-- <Signin/> -->
 <!-- <Signup/> -->
-<Navbar/>
+<!-- <Navbar/> -->
+
+<Landingpage/>
--- src/routes/Navabar/+page.svelte
+++ src/routes/Navabar/+page.svelte
@@ -7,8 +7,8 @@
     import { Gallery, Button } from 'flowbite-svelte';
   const images = [
     { alt: 'erbology', src: 'https://cdn.pixabay.com/photo/2017/12/17/08/12/girl-3023831_1280.jpg' },
-    { alt: 'shoes', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg' },
-    { alt: 'small bag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg' },
+    { alt: 'shoes', src: 'https://cdn.pixabay.com/photo/2017/12/17/08/12/girl-3023831_1280.jpg' },
+    { alt: 'small bag', src: 'https://cdn.pixabay.com/photo/2017/12/17/08/12/girl-3023831_1280.jpg' },
     { alt: 'plants', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg' },
     { alt: 'watch', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg' },
     { alt: 'shoe', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg' },
@@ -17,19 +17,16 @@
     { alt: 'lamp', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg' },
     { alt: 'toiletbag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg' },
     { alt: 'playstation', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg' },
-    { alt: 'bag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg' }
+    { alt: 'bag', src: 'https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg' },
   ];
 
-
-
-  
 
 </script>
   
 <Navbar let:hidden let:toggle>
     <NavBrand href="/">
-      <img src="" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+      <!-- <img src="" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
+      <span class="self-center whitespace-nowrap text-xl font-bold text-red-500">Photo gallery</span>
     </NavBrand>
     <NavHamburger on:click={toggle} />
  <NavUl {hidden} {activeUrl}>
@@ -68,13 +65,36 @@
 </Navbar>
 
 <div class="flex items-center justify-center py-4 md:py-8 flex-wrap gap-3 mb-3 mx-auto">
-    <Button pill size="xl" outline>All categories</Button>
-    <Button pill size="xl" color="alternative">Shoes</Button>
-    <Button pill size="xl" color="alternative">Bags</Button>
-    <Button pill size="xl" color="alternative">Electronics</Button>
-    <Button pill size="xl" color="alternative">Gaming</Button>
+    <Button pill size="xl" outline>Limkokwing 2022</Button>
+    <Button pill size="xl" color="alternative">2021</Button>
+    <Button pill size="xl" color="alternative">2022</Button>
+    <Button pill size="xl" color="alternative">2023</Button>
+    <Button pill size="xl" color="alternative">2024</Button>
+
+    <form id="upload-form" enctype="multipart/form-data" class="flex">
+        <!-- Image Upload -->
+        <div class="mb-4">
+            <label for="image" class="block font-semibold mb-2 ml-4" >Choose an image:</label>
+            <input type="file" id="image" name="image" accept="image/*" class="border border-gray-300 p-2 w-50">
+        </div>
+        
+
+        <!-- Submit Button -->
+        <div>
+            <button type="submit" class="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-full mt-10  ml-5">Upload image</button>
+        </div>
+        <!-- Caption Input -->
+        <!-- <div class="mb-4">
+            <label for="caption" class="block font-semibold mb-2" >Caption:</label>
+            <textarea id="caption" name="caption" class="border border-gray-300 p-2 w-50 h-20 resize-yes"></textarea>
+        </div> -->
+        
+    </form>
+    <!-- <Button pill size="xl" color="alternative" bg-green-100>upload</Button> -->
+    
 
     
 </div>
-  
+
+
   <Gallery items={images} class="gap-4 grid-cols-2 md:grid-cols-3" />
