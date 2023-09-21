024bb458dc9571a6b469def9c3281c01feb27bbe
Hawa <sandyhawa6@gmail.com>
2023-09-21 16:21:39 +0000

    changed the layout

--- src/routes/Landingpage/+page.svelte
+++ src/routes/Landingpage/+page.svelte
@@ -1,32 +1,42 @@
 
-<section class="bg-center bg-no-repeat bg-[url('https://cdn.pixabay.com/photo/2017/09/30/20/45/girl-2803516_1280.jpg')] bg-gray-700 bg-blend-multiply">
-  <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-      <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We share pictures to have fun</h1>
-      <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at university photo gallary we focus on showcasing the beauty of student, innovation, and capital can unlock long-term value and drive economic growth.</p>
-      <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-          <a href="/signup" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-              Get started
-              <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-              </svg>
-          </a>
-          <!-- <a href="#" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
-              Learn more
-          </a>   -->
-      </div>
+
+<div class=" max-w-full">
+
+
+  <section class="bg-center bg-cover bg-no-repeat bg-[url('https://images.pexels.com/photos/3550912/pexels-photo-3550912.jpeg?auto=compress&cs=tinysrgb&w=400')] bg-gray-700 bg-blend-multiply ">
+    <div class="px-4 mx-auto w-full text-center py-24 lg:py-56">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We share pictures to have fun</h1>
+        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at university photo gallary we focus on showcasing the beauty of student, innovation, and capital can unlock long-term value and drive economic growth.</p>
+        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <a href="/signup" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-red-900 hover:bg-red-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Get started
+                <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+            <!-- <a href="#" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                Learn more
+            </a>   -->
+        </div>
+    </div>
+  
+  </section>
+  
+  <div class="grid grid-cols-2 gap-4 bg-black " >
+    <div class="about text-gray-50 ml-15 m-auto w-2/3 text-justify">
+      <h3 class="font-bold items-center text-3xl mb-4  ">About Us</h3>
+      <p>Here at university photo gallary we focus on showcasing the beauty of student, innovation, and capital can unlock long-term value and drive economic growth Here at university photo gallary we focus on showcasing the beauty of student, innovation, and capital can unlock long-term value and drive economic growth Here at university photo gallary we focus on showcasing the beauty of student, innovation, and capital can unlock long-term value and drive economic growth Here at university photo gallary we focus on showcasing the beauty of student, innovation, and capital can unlock long-term value and drive economic growth</p>
+    </div>
+    <div class="w-2/2 m-12">
+      <img src="https://images.pexels.com/photos/5685810/pexels-photo-5685810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1 " alt=" students in class">
+  
+    </div>
   </div>
 
-</section>
 
-<div class="grid grid-cols-2 gap-4 bg-black" >
-  <div class="about text-gray-50 ml-10 m-auto">
-    <h3 class="font-bold items-center">About Us</h3>
-    <p>Here at university photo gallary we focus on showcasing the beauty of student, innovation, and capital can unlock long-term value and drive economic growth Here at university photo gallary we focus on showcasing the beauty of student, innovation, and capital can unlock long-term value and drive economic growth</p>
-  </div>
-  <div class="w-">
-    <img src="https://cdn.pixabay.com/photo/2012/02/22/20/08/classroom-15593_1280.jpg " alt=" students in class">
 
-  </div>
 </div>
+
+
 
 
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -69,7 +69,7 @@
     <div class="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24 bg-slate-900">
       <div class="mx-auto w-full max-w-sm lg:w-96">
         <div>
-          <img class="h-10 w-auto" src="" alt="Your Company">
+          <!-- <img class="h-10 w-auto" src="" alt="Your Company"> -->
           <h2 class="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-300">Create your account</h2>
         </div>
   
